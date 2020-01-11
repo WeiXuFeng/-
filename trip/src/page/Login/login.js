08f815b03890e0e6317fa6356089c11d834b0a1d
gxda <514036333@qq.com>
2020-01-11 11:01:29 +0800

    00

--- trip/src/page/Login/login.js
+++ trip/src/page/Login/login.js
@@ -7,11 +7,7 @@ import {setItem} from '../../utils/webStorage'
 class Login extends Component{
    login=()=>{
        let {validateFields} = this.props.form
-<<<<<<< HEAD
-=======
-    //    console.log(getFieldsValue())
->>>>>>> df29d6a3e5c0924bc0259bd7661ba00ef5ef04a0
-    validateFields((err,data)=>{
+     validateFields((err,data)=>{
         if(err){
             message.err('用户输入不正确')
         }else{
