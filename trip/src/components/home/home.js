08a2fb5593cc3b4574274ed29b6c7b9e6577ed76
BK1917lizhibo <1010163765@qq.com>
2020-01-11 14:24:55 +0800

    tab正确

--- trip/src/components/home/home.js
+++ trip/src/components/home/home.js
@@ -8,19 +8,19 @@ class Home extends React.Component{
             <Card title='用户管理信息'>
                 <div className={styles.box}>
                     <Button type="danger" className={styles.button} onClick={()=>{
-                        this.props.history.replace('/admin/info')
+                        this.props.history.push('/admin/info')
                     }}>网站信息</Button>
                     <Button type='primary' className={styles.button} onClick={()=>{
-                        this.props.history.replace('/admin/updateSetting')
+                        this.props.history.push('/admin/updateSetting')
                     }}>内容上传</Button>
                     <Button type="danger" className={styles.button} onClick={()=>{
-                        this.props.history.replace('/admin/role')
+                        this.props.history.push('/admin/role')
                     }}>角色管理</Button>
                     <Button type='primary' className={styles.button} onClick={()=>{
-                        this.props.history.replace('/admin/Employee')
+                        this.props.history.push('/admin/Employee')
                     }}>员工列表</Button>
                     <Button type="danger" className={styles.button} onClick={()=>{
-                        this.props.history.replace('/admin/records')
+                        this.props.history.push('/admin/records')
                     }}>数据视图</Button>
                 </div>
                 
